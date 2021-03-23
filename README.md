c55f409a386c07fa5f906cecd97c04c051e85e3f
rrose666 <uziolko@hotmail.com>
2021-03-23 15:45:39 +0000

    Added control_flow file and updated README file

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # control flow
-## if, else, elif, if else
+- if, else, elif, if else
+- Conditional statements are used to control the flow of our program
+
+### examples:
+- if:  
+  ```
+  if age >= 15:
+    print("Thank you, please proceed to your purchase")```
+  
+- elif: 
+    ```
+  elif age < 15:
+    print("You're too young to watch this movie")```  
+  
+- else:
+    ```
+  else:
+    print("something went wrong")```
+  
+## For loops and while loops:
+
+
+
---
+++ control_flow.py
@@ -0,0 +1,25 @@
+# Control flow
+# if, elif, else,
+# Conditional statements are used to control the flow of our program
+
+# Exercise:
+# user input a age and display age back to a user
+# casting implemented
+# as a user I would like to sell tickets according to
+# the age of user and category of the movie
+# age = 12, PG, 15, 18
+
+age = input("Please enter your age: ")
+# if the user is above the of 15 allow them to buy a ticket
+if int(age) >= 18:
+    print("You are:", age, "years old. You can purchase any ticket")
+# if the user is under 15 of age do not allow to buy a ticket
+elif int(age) >= 15:
+    print("You are:", age, "years old. You can purchase tickets for ratings: U, PG, 12 and 15")
+elif int(age) >= 12:
+    print("You are:", age, "years old. You can purchase tickets for ratings: U, PG and 12")
+else:
+    print("You are:", age, "years old. You can purchase tickets for ratings: U and PG")
+
+
+# for loops and while loops
